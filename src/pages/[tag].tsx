b8c4e0054c118d7bf2328eb7a363be9d03e343a8
Diego Luciano <dlucianogomez@gmail.com>
2022-08-16 19:42:12 -0400

    Fix tag route component

--- src/pages/[tag].tsx
+++ src/pages/[tag].tsx
@@ -2,20 +2,18 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react'
 
 const Redirect = () => {
-    const router = useRouter();
-
     useEffect(() => {
         const tag = window.location.href.split('/').pop();
         (async () => {
             const req = await fetch(`/api/shortener/find?tag=${tag}`);
             const res = await req.json();
             if (res === null) {
-                router.push('/');
+                window.location.assign(window.location.origin);
             } else {
                 window.location.assign(res.origin);
             }
         })();
-    }, [router]);
+    }, []);
 
     return null;
 }

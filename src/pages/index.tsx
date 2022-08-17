992e6a0c9b20a8a238659a851f159dae0e010dfb
Diego Luciano <dlucianogomez@gmail.com>
2022-08-16 20:25:03 -0400

    Fix button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
-import { FaCut, FaArrowDown } from 'react-icons/fa';
+import { FaCut, FaArrowDown, FaArrowRight } from 'react-icons/fa';
 
 const Home = () => {
   const [origin, setOrigin] = useState('');
@@ -21,17 +21,20 @@ const Home = () => {
           </Link>
         </div>
       </div>
-      <div className='w-11/12 h-auto flex flex-col items-center space-y-8'>
-        <input
-          className='w-full h-12 px-2 text-sm font-light rounded-sm border-2 border-blue-700 shadow-md bg-white placeholder-blue-200'
-          type="text"
-          placeholder={origin} />
+      <div className='w-11/12 h-auto flex flex-col items-center space-y-7'>
+        <div className='w-full h-auto flex items-center justify-between space-x-2'>
+          <input
+            className='w-9/12 h-12 px-2 text-sm font-light rounded-sm border-2 border-blue-700 shadow-md bg-white placeholder-blue-200'
+            type="text"
+            placeholder={origin} />
+          <button className='h-full w-3/12 text-sm rounded-sm font-semibold text-white bg-blue-700'>Short</button>
+        </div>
         <FaArrowDown className='text-3xl text-blue-700' />
         <input
           disabled
           className='w-full h-12 px-2 text-sm font-light rounded-sm border-2 border-blue-700 shadow-md bg-white placeholder-blue-200'
           type="text"
-          placeholder={`${origin}/vwxyz`}/>
+          placeholder={`${origin}/vwxyz`} />
       </div>
     </div>
   );

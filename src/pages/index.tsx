20c75b5990978c48b66f7d3547add40d22fe0577
Diego Luciano <dlucianogomez@gmail.com>
2022-08-16 21:13:25 -0400

    Changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,17 +16,21 @@ const Home = () => {
     if (!inputValue.length) setShortedInputValue('');
   }, [inputValue]);
 
-  const onButtonClick = async () => {
+  const onClickShortLink = async () => {
     const req = await axios.post('/api/shortener/create', { url: inputValue });
     const res = await req.data;
-    const uri = `${window.location.origin}/${res.tag}`;
+    const uri = `${origin}/${res.tag}`;
     setShortedInputValue(uri);
+  }
+
+  const onClickShortenedLink = () => {
+    navigator.clipboard.writeText(shortedInputValue);
   }
 
   return (
     <div className='w-screen h-screen flex flex-col items-center justify-center bg-gray-50'>
       <div className='fixed top-0 left-0 w-full h-16 flex items-center justify-center shadow-md bg-white'>
-        <div className='w-11/12 xl:w-9/12 h-full flex items-center justify-between'>
+        <div className='w-11/12 h-full flex items-center justify-between'>
           <Link href="/">
             <div className='w-auto flex items-center space-x-2 cursor-pointer'>
               <FaCut className='text-3xl text-blue-700' />
@@ -35,26 +39,28 @@ const Home = () => {
           </Link>
         </div>
       </div>
-      <div className='w-11/12 xl:w-9/12 h-auto flex flex-col items-center space-y-7 md:flex-row md:space-y-0 md:space-x-12'>
-        <div className='w-full h-12 flex items-center justify-between space-x-2 md:space-x-4'>
+      <div className='w-11/12 h-auto flex flex-col items-center space-y-7'>
+        <div className='w-12/12 h-12 flex items-center justify-between space-x-2'>
           <input
             value={inputValue}
             onChange={e => setInputValue(e.target.value)}
-            className='w-9/12 md:w-8/12 lg:w-9/12 h-full px-2 text-sm font-semibold text-blue-700 rounded-sm border-2 border-blue-700 shadow-md bg-white placeholder-blue-200'
+            className='w-9/12 h-full px-2 text-sm font-semibold text-blue-700 rounded-sm border-2 border-blue-700 shadow-md bg-white placeholder-blue-200'
             type="text"
             placeholder={origin} />
           <button
-            onClick={onButtonClick}
-            className='w-4/12 lg:w-3/12 h-full text-sm rounded-sm font-semibold text-white bg-blue-700'>Short it!</button>
+            onClick={onClickShortLink}
+            className='w-3/12 lg:w-3/12 h-full text-sm rounded-sm font-semibold shadow-md text-white bg-blue-700'>Short it!</button>
         </div>
         <FaArrowDown className='md:hidden text-3xl text-blue-700' />
         <FaArrowRight className='hidden md:flex text-7xl text-blue-700' />
-        <input
-          value={shortedInputValue}
-          disabled
-          className='w-full h-12 px-2 text-sm font-semibold rounded-sm border-2 border-blue-700 shadow-md text-blue-700 bg-white placeholder-blue-200 cursor-pointer'
-          type="text"
-          placeholder={`${origin}/vwxyz`} />
+        <div className='w-12/12 h-12' onClick={onClickShortenedLink}>
+          <input
+            value={shortedInputValue}
+            disabled
+            className='w-full h-full px-2 text-sm font-semibold rounded-sm border-2 border-blue-700 shadow-md text-blue-700 bg-white placeholder-blue-200 cursor-pointer'
+            type="text"
+            placeholder={`${origin}/vwxyz`} />
+        </div>
       </div>
     </div>
   );

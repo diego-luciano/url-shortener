2de171a4e333367375591f88bbe4268020c14fbb
Diego Luciano <dlucianogomez@gmail.com>
2022-08-16 21:02:28 -0400

    Add short link func to index page

--- src/pages/api/shortener/create.ts
+++ src/pages/api/shortener/create.ts
@@ -10,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         await prisma.shortened.create({
             data: { origin: req.body?.url, tag: nanoid(5) }
         })
-            .then(doc => res.status(200).json(doc))
+            .then(({tag}) => res.status(200).json({tag}))
             .catch(err => res.status(404).json(err));
     }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
+import axios from 'axios';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 import { FaCut, FaArrowDown, FaArrowRight } from 'react-icons/fa';
 
 const Home = () => {
   const [origin, setOrigin] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [shortedInputValue, setShortedInputValue] = useState('');
 
   useEffect(() => {
     setOrigin(window.location.origin);
   }, []);
+
+  useEffect(() => {
+    if (!inputValue.length) setShortedInputValue('');
+  }, [inputValue]);
+
+  const onButtonClick = async () => {
+    const req = await axios.post('/api/shortener/create', { url: inputValue });
+    const res = await req.data;
+    const uri = `${window.location.origin}/${res.tag}`;
+    setShortedInputValue(uri);
+  }
 
   return (
     <div className='w-screen h-screen flex flex-col items-center justify-center bg-gray-50'>
@@ -16,7 +30,7 @@ const Home = () => {
           <Link href="/">
             <div className='w-auto flex items-center space-x-2 cursor-pointer'>
               <FaCut className='text-3xl text-blue-700' />
-              <p className='font-medium text-xl text-blue-700'>Short it</p>
+              <p className='font-semibold text-xl text-blue-700'>Short it</p>
             </div>
           </Link>
         </div>
@@ -24,16 +38,21 @@ const Home = () => {
       <div className='w-11/12 xl:w-9/12 h-auto flex flex-col items-center space-y-7 md:flex-row md:space-y-0 md:space-x-12'>
         <div className='w-full h-12 flex items-center justify-between space-x-2 md:space-x-4'>
           <input
-            className='w-9/12 md:w-8/12 lg:w-9/12 h-full px-2 text-sm font-light rounded-sm border-2 border-blue-700 shadow-md bg-white placeholder-blue-200'
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+            className='w-9/12 md:w-8/12 lg:w-9/12 h-full px-2 text-sm font-semibold text-blue-700 rounded-sm border-2 border-blue-700 shadow-md bg-white placeholder-blue-200'
             type="text"
             placeholder={origin} />
-          <button className='w-4/12 lg:w-3/12 h-full text-sm rounded-sm font-semibold text-white bg-blue-700'>Short it!</button>
+          <button
+            onClick={onButtonClick}
+            className='w-4/12 lg:w-3/12 h-full text-sm rounded-sm font-semibold text-white bg-blue-700'>Short it!</button>
         </div>
         <FaArrowDown className='md:hidden text-3xl text-blue-700' />
         <FaArrowRight className='hidden md:flex text-7xl text-blue-700' />
         <input
+          value={shortedInputValue}
           disabled
-          className='w-full h-12 px-2 text-sm font-light rounded-sm border-2 border-blue-700 shadow-md bg-white placeholder-blue-200 cursor-pointer'
+          className='w-full h-12 px-2 text-sm font-semibold rounded-sm border-2 border-blue-700 shadow-md text-blue-700 bg-white placeholder-blue-200 cursor-pointer'
           type="text"
           placeholder={`${origin}/vwxyz`} />
       </div>
